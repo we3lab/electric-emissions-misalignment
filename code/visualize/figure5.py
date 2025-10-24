a2e3cb22c3287a203055586ff06d14168683f8d3
Fletcher Chapin <fletchapin@gmail.com>
2025-10-24 14:49:09 -0700

    Updated Figure 5 to remove seasonality

--- code/visualize/figure5.py
+++ code/visualize/figure5.py
@@ -36,33 +36,6 @@ regions = ["CAISO", "ERCOT", "ISONE", "MISO", "NYISO", "PJM", "SPP"]
 # locate the data folders
 corr_dir = os.path.join(basepath, "data/correlation")
 
-month_season_map = {
-    1: "Winter",
-    2: "Winter",
-    3: "Winter",
-    4: "Winter",
-    5: "Summer",
-    6: "Summer",
-    7: "Summer",
-    8: "Summer",
-    9: "Summer",
-    10: "Summer",
-    11: "Winter",
-    12: "Winter",
-}
-
-region_color_map = {
-    "CAISO": "#66c2a5",
-    "ERCOT": "#fc8d62",
-    "ISONE": "#8da0cb",
-    "MISO": "#a6d854",
-    "NYISO": "#ffd92f",
-    "PJM": "#e5c494",
-    "SPP": "#b3b3b3",
-}
-
-shapes = ["s", "o", "D", "v", "P", "*", "X"]
-regions = ["CAISO", "ERCOT", "ISONE", "MISO", "NYISO", "PJM", "SPP"]
 months = [
     "Jan",
     "Feb",
@@ -77,106 +50,60 @@ months = [
     "Nov",
     "Dec",
 ]
-region_shape_dict = dict(zip(regions, shapes))
 
-new_aef_tariff_data = {"iso": [], "corr_coef": [], "month": [], "season": []}
-new_mef_dam_data = {"iso": [], "corr_coef": [], "month": [], "season": []}
+combined_data = {"iso": [], "corr_coef": [], "month": [], "price_type": []}
 
 for region in regions:
     aef_corr_df = pd.read_csv(os.path.join(corr_dir, region + "_aef_pearson.csv"))
     mef_corr_df = pd.read_csv(os.path.join(corr_dir, region + "_mef_pearson.csv"))
     for month in range(1, 13):
-        season = month_season_map[month]
         # AEF/tariff correlation
         for index, row in aef_corr_df.iterrows():
             if (math.isclose(row[months[month - 1]], 0, abs_tol=1e-8) or pd.isna(row[months[month - 1]])):
                 pass  # zero correlation due to flat tariff
             else:
-                new_aef_tariff_data["iso"].append(region)
-                new_aef_tariff_data["season"].append(season)
-                new_aef_tariff_data["month"].append(month)
-                new_aef_tariff_data["corr_coef"].append(row[months[month - 1]])
+                combined_data["iso"].append(region)
+                combined_data["month"].append(month)
+                combined_data["corr_coef"].append(row[months[month - 1]])
+                combined_data["price_type"].append("Tariff/AEF")
         # MEF/DAM correlation
         for index, row in mef_corr_df[mef_corr_df["month"] == month].iterrows():
-            new_mef_dam_data["iso"].append(region)
-            new_mef_dam_data["season"].append(season)
-            new_mef_dam_data["month"].append(month)
-            new_mef_dam_data["corr_coef"].append(row["pearson_cc"])
+            combined_data["iso"].append(region)
+            combined_data["month"].append(month)
+            combined_data["corr_coef"].append(row["pearson_cc"])
+            combined_data["price_type"].append("DAM/MEF")
 
 ## Create Subplots
 # create all plots on a single subplot
 # 1-column width = 80 mm
 # 2-column width = 190 mm
 # max height is 240 mm
-fig, ax = plt.subplots(2, 1, figsize=(80 / 25.4, 120 / 25.4))
+fig, ax = plt.subplots(1, 1, figsize=(80 / 25.4, 60 / 25.4))
 
 ## Subplot A
-df = pd.DataFrame(new_aef_tariff_data)
-seasonal_colors = ["#5E9BA1", "#A1645E"]
-bright_seasonal_colors = ["#71bac1", "#c17871"]
-sns.set_palette(bright_seasonal_colors)
-ax[0].axhline(0, linestyle="dotted", color="grey")
+df = pd.DataFrame(combined_data)
+colors = ["#FFE599", "#FAB477"]
+bright_colors = ["#FFDF84", "#FF8C5D"]
+sns.set_palette(bright_colors)
+
+ax.axhline(0, linestyle="dotted", color="grey")
 plot_a = sns.violinplot(
     data=df,
     x="iso",
     y="corr_coef",
-    hue="season",
+    hue="price_type",
     linewidth=0.0,
     width=0.75,
     density_norm="width",
-    ax=ax[0],
+    ax=ax,
     inner="box",
     inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
 )
-ax[0].set_xlabel("")
-ax[0].set_ylim(-1, 1)
-ax[0].set_ylabel("Pearson Correlation Coefficient")
+ax.set_xlabel("")
+ax.set_ylim(-1, 1)
+ax.set_ylabel("Pearson Correlation Coefficient")
 # plt.title("AEF vs. Tariffs")
-ax[0].legend(loc="lower right", frameon=False)
-
-## Subplot B
-df = pd.DataFrame(new_mef_dam_data)
-seasonal_colors = ["#5E9BA1", "#A1645E"]
-bright_seasonal_colors = ["#71bac1", "#c17871"]
-sns.set_palette(bright_seasonal_colors)
-ax[1].axhline(0, linestyle="dotted", color="grey")
-plot_c = sns.violinplot(
-    data=df,
-    x="iso",
-    y="corr_coef",
-    hue="season",
-    linewidth=0.0,
-    width=0.75,
-    density_norm="width",
-    ax=ax[1],
-    inner="box",
-    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
-)
-ax[1].set_xlabel("")
-ax[1].set_ylim(-1, 1)
-ax[1].set_ylabel("Pearson Correlation Coefficient")
-# plt.title("MEF vs. DAM Prices")
-ax[1].legend(loc="lower right", frameon=False)
-
-## Save Outputs
-labels = ["a.", "b."]
-
-# from https://matplotlib.org/stable/gallery/text_labels_and_annotations/label_subplots.html
-for label, axis in zip(labels, ax.flatten()):
-    # Use ScaledTranslation to put the label
-    # - at the top left corner (axes fraction (0, 1)),
-    # - offset 20 pixels left and 7 pixels up (offset points (-20, +7)),
-    # i.e. just outside the axes.
-    axis.text(
-        0.0,
-        1.0,
-        label,
-        transform=(
-            axis.transAxes + ScaledTranslation(-36 / 72, 0, fig.dpi_scale_trans)
-        ),
-        va="bottom",
-        fontsize=10,
-    )
+ax.legend(loc="lower right", frameon=False)
 
 fig.tight_layout()
 fig_path = os.path.join(basepath, "figures")
