c91d5b63f82250e6f477c4b5390350c28eaea766
Fletcher Chapin <fletchapin@gmail.com>
2026-01-12 15:42:45 -0800

    Created supplementary figure 7

--- README.md
+++ README.md
@@ -23,6 +23,10 @@ Some data cannot be republished under MIT license, in which case links to the da
     - Census boundaries for IBDR display from [Census.gov](https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html)
     - Independent System Operator (ISO) shapefile from US Department of Homeland Security's [Homeland Infrastructure Foundation-Level Data (HIFLD)](https://hifld-geoplatform.hub.arcgis.com/datasets/50f80920d36e435d9a34db2bd0fd3ad8_0/explore).
         - ***NOTE***: This dataset is no longer publicly accessible, but similar data is available through EIA's [U.S. Energy Atlas](https://atlas.eia.gov/search) under [RTO Regions](https://atlas.eia.gov/datasets/b286c693074045b3ac9b5d7300162e99_256/explore?location=35.167089%2C-95.679808%2C3.72).
+- *Load profiles*:
+    - Load profiles were the clusters from the [Elmas dataset](https://doi.org/10.6084/m9.figshare.23889780) [5]
+       - Flat load profiles are the default (`col_idx=0`) and the other 18 clusters are described in the Elmas data descriptor.
+       - All load profiles are normalized to 1 MW.
 
 ## Data preprocessing
 
@@ -121,3 +125,5 @@ figures
 [3] Chapin, F. T., Rao, A. K., Sakthivelu, A., Chen, C. S., & Mauter, M. S. (2025). *Industrial and Commercial Electricity Tariffs in the United States* (Version 2023.06.01) [Data set]. Zenodo. https://doi.org/10.5281/zenodo.16739989
 
 [4] David, E., Sakthivelu, A., Rao, A. K., & Mauter, M. S. (2024). *US incentive based demand response program parameters* (Version 2) [Data set]. https://doi.org/10.25740/ck480bd0124
+
+[5] Bellinguer, K., Girard, R., Bocquet, A., & Chevalier, A. (2023). ELMAS: a one-year dataset of hourly electrical load profiles from 424 French industrial and tertiary sectors. *Scientific Data, 10*(1), 686. https://doi.org/10.1038/s41597-023-02542-z
--- code/visualize/supplementary7.py
+++ code/visualize/supplementary7.py
@@ -31,22 +31,19 @@ plt.rcParams.update(
     }
 )
 
-regions = ["CAISO", "ERCOT", "ISONE", "MISO", "NYISO", "PJM", "SPP"]
-
 # locate the data folders
 corr_dir = os.path.join(basepath, "data/correlation")
 
-region_color_map = {
-    "CAISO": "#66c2a5",
-    "ERCOT": "#fc8d62",
-    "ISONE": "#8da0cb",
-    "MISO": "#a6d854",
-    "NYISO": "#ffd92f",
-    "PJM": "#e5c494",
-    "SPP": "#b3b3b3",
+sector_color_map = {
+    "Flat": "#66c2a5",
+    "Manufact\n-uring": "#fc8d62",
+    "Trades\n(non-food)": "#8da0cb",
+    "Office": "#a6d854",
+    "Water &\ntelecom": "#ffd92f",
+    "Crops &\ntransport": "#e5c494",
 }
 
-sectors = ["Flat", ]
+sectors = ["Flat", "Manufact\n-uring", "Trades\n(non-food)", "Office", "Water &\ntelecom", "Crops &\ntransport"]
 regions = ["CAISO", "ERCOT", "ISONE", "MISO", "NYISO", "PJM", "SPP"]
 months = [
     "Jan",
@@ -63,80 +60,188 @@ months = [
     "Dec",
 ]
 
-
 combined_data = {"iso": [], "corr_coef": [], "month": [], "sector": []}
 
 for region in regions:
-    if sector = "Flat":
-        aef_corr_df = pd.read_csv(os.path.join(corr_dir, region + "_aef_pearson.csv"))
-    for month in range(1, 13):
-        # AEF/tariff correlation
-        for index, row in aef_corr_df.iterrows():
-            if (math.isclose(row[months[month - 1]], 0, abs_tol=1e-8) or pd.isna(row[months[month - 1]])):
-                pass  # zero correlation due to flat tariff
-            else:
-                combined_data["iso"].append(region)
-                combined_data["month"].append(month)
-                combined_data["corr_coef"].append(row[months[month - 1]])
-                combined_data["sector"].append(sector)
+    for sector in sectors:
+        if sector == "Flat":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir, region + "_aef_pearson.csv"))
+        elif sector == "Manufact\n-uring":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir, "supplementary", region + "_aef_pearson (1).csv"))
+        elif sector == "Trades\n(non-food)":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir,  "supplementary", region + "_aef_pearson (2).csv"))
+        elif sector == "Office":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir, "supplementary", region + "_aef_pearson (5).csv"))
+        elif sector == "Crops &\ntransport":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir, "supplementary", region + "_aef_pearson (6).csv"))
+        elif sector == "Water &\ntelecom":
+            aef_corr_df = pd.read_csv(os.path.join(corr_dir, "supplementary", region + "_aef_pearson (8).csv"))
+        else:
+            raise ValueError(f"Sector {sector} not in sectors: {sectors}")
+        
+        for month in range(1, 13):
+            # AEF/tariff correlation
+            for index, row in aef_corr_df.iterrows():
+                if (math.isclose(row[months[month - 1]], 0, abs_tol=1e-8) or pd.isna(row[months[month - 1]])):
+                    pass  # zero correlation due to flat tariff
+                else:
+                    combined_data["iso"].append(region)
+                    combined_data["month"].append(month)
+                    combined_data["corr_coef"].append(row[months[month - 1]])
+                    combined_data["sector"].append(sector)
+
+df = pd.DataFrame(combined_data)
 
 ## Create Subplots
 # create all plots on a single subplot
 # 1-column width = 80 mm
 # 2-column width = 190 mm
 # max height is 240 mm
-fig, ax = plt.subplots(2, 1, figsize=(80 / 25.4, 120 / 25.4))
+fig, ax = plt.subplots(4, 2, figsize=(190 / 25.4, 240 / 25.4))
 
-## Subplot A
-df = pd.DataFrame(new_aef_tariff_data)
-seasonal_colors = ["#5E9BA1", "#A1645E"]
-bright_seasonal_colors = ["#71bac1", "#c17871"]
-sns.set_palette(bright_seasonal_colors)
-ax[0].axhline(0, linestyle="dotted", color="grey")
+## Subplot A: CAISO
+sns.set_palette(sector_color_map.values())
+ax[0,0].axhline(0, linestyle="dotted", color="grey")
 plot_a = sns.violinplot(
-    data=df,
-    x="iso",
+    data=df[df["iso"] == "CAISO"],
+    x="sector",
     y="corr_coef",
-    hue="season",
+    hue="sector",
     linewidth=0.0,
     width=0.75,
     density_norm="width",
-    ax=ax[0],
+    ax=ax[0,0],
     inner="box",
     inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
 )
-ax[0].set_xlabel("")
-ax[0].set_ylim(-1, 1)
-ax[0].set_ylabel("Pearson Correlation Coefficient")
-# plt.title("AEF vs. Tariffs")
-ax[0].legend(loc="lower right", frameon=False)
+ax[0,0].set_xlabel("")
+ax[0,0].set_ylim(-1, 1)
+ax[0,0].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[0,0].legend(loc="lower right", frameon=False)
 
-## Subplot B
-df = pd.DataFrame(new_mef_dam_data)
-seasonal_colors = ["#5E9BA1", "#A1645E"]
-bright_seasonal_colors = ["#71bac1", "#c17871"]
-sns.set_palette(bright_seasonal_colors)
-ax[1].axhline(0, linestyle="dotted", color="grey")
-plot_c = sns.violinplot(
-    data=df,
-    x="iso",
+## Subplot B: ERCOT
+ax[0,1].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "ERCOT"],
+    x="sector",
     y="corr_coef",
-    hue="season",
+    hue="sector",
     linewidth=0.0,
     width=0.75,
     density_norm="width",
-    ax=ax[1],
+    ax=ax[0,1],
     inner="box",
     inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
 )
-ax[1].set_xlabel("")
-ax[1].set_ylim(-1, 1)
-ax[1].set_ylabel("Pearson Correlation Coefficient")
-# plt.title("MEF vs. DAM Prices")
-ax[1].legend(loc="lower right", frameon=False)
+ax[0,1].set_xlabel("")
+ax[0,1].set_ylim(-1, 1)
+ax[0,1].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[0,1].legend(loc="lower right", frameon=False)
+
+## Subplot C: ISONE
+ax[1,0].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "ISONE"],
+    x="sector",
+    y="corr_coef",
+    hue="sector",
+    linewidth=0.0,
+    width=0.75,
+    density_norm="width",
+    ax=ax[1,0],
+    inner="box",
+    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
+)
+ax[1,0].set_xlabel("")
+ax[1,0].set_ylim(-1, 1)
+ax[1,0].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[1,0].legend(loc="lower right", frameon=False)
+
+## Subplot D: MISO
+ax[1,1].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "MISO"],
+    x="sector",
+    y="corr_coef",
+    hue="sector",
+    linewidth=0.0,
+    width=0.75,
+    density_norm="width",
+    ax=ax[1,1],
+    inner="box",
+    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
+)
+ax[1,1].set_xlabel("")
+ax[1,1].set_ylim(-1, 1)
+ax[1,1].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[1,1].legend(loc="lower right", frameon=False)
+
+## Subplot E: NYISO
+ax[2,0].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "NYISO"],
+    x="sector",
+    y="corr_coef",
+    hue="sector",
+    linewidth=0.0,
+    width=0.75,
+    density_norm="width",
+    ax=ax[2,0],
+    inner="box",
+    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
+)
+ax[2,0].set_xlabel("")
+ax[2,0].set_ylim(-1, 1)
+ax[2,0].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[2,0].legend(loc="lower right", frameon=False)
+
+## Subplot F: PJM
+ax[2,1].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "PJM"],
+    x="sector",
+    y="corr_coef",
+    hue="sector",
+    linewidth=0.0,
+    width=0.75,
+    density_norm="width",
+    ax=ax[2,1],
+    inner="box",
+    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
+)
+ax[2,1].set_xlabel("")
+ax[2,1].set_ylim(-1, 1)
+ax[2,1].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[2,1].legend(loc="lower right", frameon=False)
+
+## Subplot G: SPP
+ax[3,0].axhline(0, linestyle="dotted", color="grey")
+plot_a = sns.violinplot(
+    data=df[df["iso"] == "SPP"],
+    x="sector",
+    y="corr_coef",
+    hue="sector",
+    linewidth=0.0,
+    width=0.75,
+    density_norm="width",
+    ax=ax[3,0],
+    inner="box",
+    inner_kws={"box_width": 2.0, "marker": '.', "markersize": 3},
+)
+ax[3,0].set_xlabel("")
+ax[3,0].set_ylim(-1, 1)
+ax[3,0].set_ylabel("Pearson\nCorrelation Coefficient")
+ax[3,0].legend(loc="lower right", frameon=False)
+
+# Remove the frame from the unused subplot
+ax[3,1].set_frame_on(False)
+ax[3,1].tick_params(axis='x', which='both', length=0)
+ax[3,1].tick_params(axis='x', labelbottom=False)
+ax[3,1].tick_params(axis='y', which='both', length=0)
+ax[3,1].tick_params(axis='y', labelleft=False)
 
 ## Save Outputs
-labels = ["a.", "b."]
+labels = ["a.", "b.", "c.", "d.", "e.", "f.", "g."]
 
 # from https://matplotlib.org/stable/gallery/text_labels_and_annotations/label_subplots.html
 for label, axis in zip(labels, ax.flatten()):
